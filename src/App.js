b095da40189b8f61641f68135d4cae96adb4e810
David Sires <d.sires@yahoo.com>
2017-11-05 11:29:55 -0800

    Firebase Install

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      speed: 10
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const speedRef = rootRef.child('speed');
+    speedRef.on('value', snap => {
+      this.setState({
+        speed: snap.val()
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +29,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.speed}</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var config = {
+    apiKey: "AIzaSyDpfdVJnQZvyrJ9Oz1kCl4S-3iN97uZ9zo",
+    authDomain: "blocitoff-c5fb1.firebaseapp.com",
+    databaseURL: "https://blocitoff-c5fb1.firebaseio.com",
+    projectId: "blocitoff-c5fb1",
+    storageBucket: "blocitoff-c5fb1.appspot.com",
+    messagingSenderId: "488274149986"
+  };
+  firebase.initializeApp(config);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
