5265f4c7247b997a9770d4e09b50020b9d615c97
David Sires <d.sires@yahoo.com>
2017-11-11 12:07:52 -0800

    Hide-Old-Tasks

--- src/App.js
+++ src/App.js
@@ -15,10 +15,15 @@ class App extends Component {
   componentWillMount() {
     let tasksRef = firebase.database().ref('tasks').orderByKey().limitToLast(100);
     tasksRef.on('child_added', snap => {
-      let task = {
-        text: snap.val(),
-        id: snap.key
+      let task = snap.val();
+      //create a timer to remove tasks older than 7 days in milliseconds
+      if(task.setTime < (Date.now() - 604800000)) {
+        firebase.database().ref('tasks').child(snap.key).update({
+          expired: true
+        });
       };
+      task.expiredString = String(task.expired);
+      task.id = snap.key
       this.setState({
         tasks: [task].concat(this.state.tasks)
       });
@@ -27,7 +32,14 @@ class App extends Component {
 
   addTask(e) {
     e.preventDefault();
-    firebase.database().ref('tasks').push( this.inputEl.value );
+    let todo = {
+      text: this.inputEl.value,
+      createAt: Date(),
+      setTime: Date.now(),
+      completed: false,
+      expired: false
+    }
+    firebase.database().ref('tasks').push( todo );
     this.inputEl.value = '';
   }
 
@@ -42,10 +54,29 @@ class App extends Component {
         <form onSubmit={this.addTask.bind(this)}>
           <input type="text" ref={ el => this.inputEl = el } />
           <input type="submit" />
-          <div>
-            <ul>
+          <div className="completeList">
+            <h1>Tasks to be completed!</h1>
+            <ul className="uList">
               {
-                this.state.tasks.map( task => <li key={task.id}>{task.text}</li>)
+                this.state.tasks.filter(
+                task => !task.expired ).map(
+                task => <li className="liList" key={task.id}>
+                          {task.text} - Created:
+                          {task.createAt}
+                        </li> )
+              }
+            </ul>
+          </div>
+          <div className="expiredList">
+            <h2>Expired Tasks!</h2>
+            <ul className="uList">
+              {
+                this.state.tasks.filter(
+                task => task.expired ).map(
+                task => <li className="liList" key={task.id}>
+                          {task.text} - Create:
+                          {task.createAt}
+                        </li> )
               }
             </ul>
           </div>
